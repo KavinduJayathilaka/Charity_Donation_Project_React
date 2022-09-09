468453a991a3d6db9370176e2505e4fb27e65646
KavinduJayathilaka <70713220+KavinduJayathilaka@users.noreply.github.com>
2022-09-09 23:29:05 +0530

    Basic done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Body from "./container/Body/Body";
+import Header from "./container/Header/Header";
+
+const App = () => (
+  <div className="app">
+    <Navbar></Navbar>
+    <Header></Header>
+    <Body></Body>
+  </div>
+);
+export default App;
